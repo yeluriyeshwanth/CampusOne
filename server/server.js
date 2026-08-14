d32dbe1154a700e69aebd4b6e00ae18726f3b37a
yeuriyeshwanth <yeluriyeshwanth@gmail.com>
2026-08-14 21:13:25 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -73,6 +73,6 @@ app.get('/api/test', (req, res) => {
 // Server port
 const PORT = process.env.PORT || 5001
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
